29200a1a5dc12919d207d0cb6d2af65b982e7f04
Illia Lubenets <illialubenets@yandex.ua>
2016-01-17 17:33:52 +0200

    FirstVersion

---
+++ DevelopexFinal/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DevelopexFinal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TextSearch textSearch;
+        public MainWindow()
+        {
+            InitializeComponent();
+            textSearch = new TextSearch();
+            button1.IsEnabled = false;
+            button2.IsEnabled = false;
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            string url = textBox.Text;
+            int threadNum;
+            string text = textBox2.Text;
+            int linkNum;
+
+            if (url == "")
+            {
+                textBlock4.Text = "Havent link";
+            }
+            else
+            if ((!Int32.TryParse(textBox1.Text, out threadNum))&&(threadNum<1))
+            {
+                textBlock4.Text = "Wrong Number of Threads";
+            }
+            else
+            if (text == "")
+            {
+                textBlock4.Text = "Write text for search";
+            }
+            else
+            if ((!Int32.TryParse(textBox3.Text, out linkNum))&&(linkNum<1))
+            {
+                textBlock4.Text = "Wrong Number of Links";
+            }
+            else
+            {
+                button.IsEnabled = false;
+                button1.IsEnabled = true;
+                button2.IsEnabled = true;
+                textBlock4.Text = "Loading";
+                textSearch.Start(this, url, threadNum, text, linkNum);
+
+            }
+        }
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            button.IsEnabled = true;
+            button1.IsEnabled = false;
+            button2.IsEnabled = false;
+            textBlock4.Text = "Stoped";
+            textSearch.Stop();
+        }
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            button.IsEnabled = true;
+            button1.IsEnabled = false;
+            button2.IsEnabled = false;
+            textBlock4.Text = "Paused";
+            textSearch.Pause();
+        }
+    }
+}
---
+++ DevelopexFinal/TextSearch.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace DevelopexFinal
+{
+    class TextSearch
+    {
+        int status=0; //0=stop 1=work 2=pause
+        int curentEl;
+        const string linkpattern = @"http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?";
+        object lockCurentEl = new object();
+        int total;
+        object lockTotal = new object();
+        int threadsNum;
+        string text;
+        int linkNum;
+        int linkComplete;
+        object locklinkComplete = new object();
+        MainWindow window;
+        public List<LinkStat> links { get; set; }
+        Thread[] threads;
+        Thread mainThread;
+
+        string DeleteTags(string html)
+        {
+            string noHTML = Regex.Replace(html, @"<[^>]+>|&nbsp;", "").Trim();
+            string noHTMLNormalised = Regex.Replace(noHTML, @"\s{2,}", " ");
+            return noHTMLNormalised;
+        }
+        void SearchThread()
+        {
+            while  (linkComplete < total)
+            {
+                bool work = false;
+                LinkStat link = new LinkStat();
+                lock (lockCurentEl)
+                {
+                    if (curentEl < total)
+                    {
+                        link = links[curentEl];
+                        curentEl++;
+                        work = true;
+                    }
+                }
+                if (work)
+                {
+                    link.Status = "Loading";
+                    WebClient web = new WebClient();
+                    try
+                    {
+                        string html = web.DownloadString(link.Link);
+                        string nohtml = DeleteTags(html);
+                        if (nohtml.IndexOf(text) == -1)
+                        {
+                            link.Status = "Not fond";
+                        }
+                        else
+                        {
+                            link.Status = "Find";
+                        }
+                        Match m = Regex.Match(html, linkpattern);
+                        while (m.Success)
+                        {
+                            lock (lockTotal)
+                            {
+                                if (total < linkNum)
+                                {
+                                    links.Add(new LinkStat() { Link = m.Value, Status = "Wait" });
+                                    total++;
+                                    m = m.NextMatch();
+                                }
+                                else
+                                    break;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        link.Status = ex.Message;
+                    }
+                    finally
+                    {
+                        lock (locklinkComplete)
+                        {
+                            linkComplete++;
+                        }
+                    }
+                }
+
+            }
+        }
+       public void Start(MainWindow win,string url, int thrNum, string searchText, int urlNum )
+        {
+
+            if (status == 0)
+            {
+                threadsNum = thrNum;
+                text = searchText;
+                linkNum = urlNum;
+                links = new List<LinkStat>(linkNum);
+                LinkStat link = new LinkStat() { Link = url, Status = "Wait" };
+                links.Add(link);
+                total = 1;
+                curentEl = 0;
+                window = win;
+                mainThread = new Thread(ControlThread);
+                mainThread.Start();
+                threads = new Thread[threadsNum];
+                for (int i = 0; i < threadsNum; i++)
+                {
+                    threads[i] = new Thread(SearchThread);
+                    threads[i].Start();
+                }
+            }
+            else
+            {
+                mainThread.Resume();
+                for (int i = 0; i < threadsNum; i++)
+                {
+                    threads[i].Resume();
+                }
+            }
+           
+
+        }
+        public void ControlThread()
+        {
+            while(linkComplete< total)
+            {
+                Thread.Sleep(500);
+                window.Dispatcher.Invoke(delegate
+                {
+                    window.listView.ItemsSource = links;
+                    window.listView.Items.Refresh();
+                    window.Progres.Maximum = linkNum;
+                    window.Progres.Value = linkComplete;
+                });
+            }
+            window.Dispatcher.Invoke(delegate
+            {
+                window.listView.Items.Refresh();
+                window.Progres.Maximum = linkNum;
+                window.Progres.Value = linkComplete;
+                window.textBlock4.Text = "Loaded";
+                window.button1.IsEnabled = false;
+                window.button.IsEnabled = true;
+                window.button2.IsEnabled = false;
+            });
+            status = 0;
+        }
+        public void Stop()
+        {
+            status = 0;
+            
+            for(int i=0;i<threadsNum;i++)
+            {
+                threads[i].Abort(); 
+            }
+            mainThread.Abort();
+        }
+        public void Pause()
+        {
+            
+                status = 2;
+            
+            for (int i = 0; i < threadsNum; i++)
+            {
+                threads[i].Suspend();
+            }
+            mainThread.Suspend();
+        }
+
+    }
+}
